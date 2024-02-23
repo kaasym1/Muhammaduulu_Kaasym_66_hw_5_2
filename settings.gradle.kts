1d5ce829f29101ab0e43c41bbd8bdd21d568b78b
kaasym1 <kasymmuhammaduulu@gmail.com>
2024-02-23 17:38:12 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Muhammaduulu_Kaasym_66_hw_5_2"
+include(":app")
